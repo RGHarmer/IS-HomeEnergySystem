1585ffbe729892ca2300116a24d892b2da9ec729
RGHarmer <jebkermanftw@gmail.com>
2018-09-25 13:39:16 +1000

    Communication Skeleton

--- src/agents/ApplianceAgent.java
+++ src/agents/ApplianceAgent.java
@@ -1,7 +1,59 @@
 package agents;
 
 import jade.core.Agent;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREResponder;
 
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Vector;
+ 
 public class ApplianceAgent extends Agent {
-
-}
+	private Map<Instant,Float> archive;
+	private float currentEstimate;
+	private float currentUsage;
+	
+	protected void setup() {
+		MessageTemplate template = MessageTemplate.and(MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST),
+		  		MessageTemplate.MatchPerformative(ACLMessage.REQUEST) );
+		
+		addBehaviour(new AchieveREResponder(this, template) {
+			protected ACLMessage prepareResponse(ACLMessage request) throws NotUnderstoodException, RefuseException {
+				
+				// Actions
+				
+				if (new Boolean(false)) { // Agent refuses
+					throw new RefuseException("[REFUSE MESSAGE]");
+				}
+				else {
+					ACLMessage agree = request.createReply();
+					agree.setPerformative(ACLMessage.AGREE);
+					return agree;
+				}
+			}
+			
+			protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) throws FailureException {
+				
+				// Actions
+				
+				if (new Boolean(false)) { //
+					throw new FailureException("[FAIL MESSAGE]");
+				}
+				else {
+					ACLMessage inform = request.createReply();
+					inform.setPerformative(ACLMessage.INFORM);
+					return inform;
+				}
+			}
+		});
+	}
+	
+	
+	
+} 
--- src/agents/RetailerAgent.java
+++ src/agents/RetailerAgent.java
@@ -1,7 +1,49 @@
 package agents;
 
 import jade.core.Agent;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
 
 public abstract class RetailerAgent extends Agent {
-
+	
+	protected void setup() {
+		MessageTemplate template = MessageTemplate.and(
+				MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET),
+				MessageTemplate.MatchPerformative(ACLMessage.CFP));
+		
+		addBehaviour(new ContractNetResponder(this, template) {
+			protected ACLMessage prepareResponse(ACLMessage cfp) throws NotUnderstoodException, RefuseException {
+				if (new Boolean(false)) { //Something went wrong or agent refuses
+					throw new RefuseException("[REFUSE MESSAGE]");
+				}
+				else {
+					ACLMessage propose = cfp.createReply();
+					propose.setPerformative(ACLMessage.PROPOSE);
+					propose.setContent("[PROPOSAL CONTENT]");
+					return propose;
+				}
+			}
+			
+			protected ACLMessage prepareResultNotification(ACLMessage cfp, ACLMessage propose, ACLMessage accept) throws FailureException{
+				if (new Boolean(false)) { //Something went wrong
+					throw new FailureException("[FAIL MESSAGE]");
+				}
+				else {
+					ACLMessage inform = accept.createReply();
+					inform.setPerformative(ACLMessage.PROPOSE);
+					return inform;
+				}
+			}
+			
+			protected void handleRejectProposal(ACLMessage reject) {
+				
+			}
+		});
+	}
+	
 }
