5d167824de833bc45ebb40f91a51fb4ca1fc2143
SolarZephyr <7666527@student.swin.edu.au>
2018-09-25 16:51:42 +1000

    25 SEP 2018 : JD : Added inline documentation for AddEnergy() method, feel free to change the name of this method.

--- src/agents/HomeAgent.java
+++ src/agents/HomeAgent.java
@@ -13,7 +13,13 @@ public class HomeAgent extends Agent {
 	private Clock clock;
 	private int cycleInterval = 10000; //in milliseconds
 	private MessageTicker ticker;
-	
+	/**
+	 * This method adds an energy usage block to the map, using the associated Appliance Agent ID
+	 * 
+	 * @param id This is the Agent ID
+	 * @param usage This is the energy usage for the current Cycle
+	 * @param timestamp this is the associated Instant/Timestamp
+	 */
 	public void AddEnergyUsage(AID id, float usage, Instant timestamp) {
 		//Log stuff
 		
